dbe60bd23a084234d559cccc4c845873504cf83f
Muhammad-Talha <muhammad.talha.776932@gmail.com>
2023-06-22 15:33:08 +0500

    startup step of node-ts

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import * as dotenv from 'dotenv';
+import express, { Request, Response, Express, NextFunction } from 'express';
+import cors from 'cors'
+
+
+// * Config the .env file here
+dotenv.config();
+
+// * create the instence of Express 
+const app: Express = express();
+
+// * setup the middleware 
+
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+
+
+
+// ? handle the default route get request and response
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({ message: "Hello from DALL.E" });
+});
+
+
+// * use the dalle route over here
+
+
+
+
+// * Listen the server at localhost:3000
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
